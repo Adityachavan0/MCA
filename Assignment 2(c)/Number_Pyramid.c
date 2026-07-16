5be0f88466826a9cdb3e565f668477ed9a173311
AdiChavan08 <130481230+Adityachavan0@users.noreply.github.com>
2026-07-16 20:19:44 +0530

    Add Number Pyramid pattern program
    Implement a program to print a number pyramid pattern based on user input.

---
+++ Assignment 2(c)/Number_Pyramid.c
@@ -0,0 +1,33 @@
+/*Pattern:-
+    1
+   121
+  12321
+ 1234321
+123454321
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n ;
+    int i, j,k;
+    printf("Enter value of n :");
+    scanf("%d",&n);
+    for (i = 1; i <= n; i++) {
+        for (j = n; j >i; j--) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        if(i>1)
+        for(k=i-1;k>=1;k--){
+            printf("%d", k);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}

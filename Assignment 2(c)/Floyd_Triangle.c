f708cdec9cdfdaf39ede5acd6ce8580ec72a4334
AdiChavan08 <130481230+Adityachavan0@users.noreply.github.com>
2026-07-16 20:02:16 +0530

    Create Floyd_Triangle.c

---
+++ Assignment 2(c)/Floyd_Triangle.c
@@ -0,0 +1,21 @@
+/*Pattern:-
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15
+*/
+#include<stdio.h>
+int main(){
+  int n;
+  printf("Enter value of n");
+  scanf("%d",&n);
+  int c=1;
+  for(int i=1;i<=n;i++){
+      for(int j=1;j<=i;j++){
+          printf("%d ",c++);
+      }
+      printf("\n");
+  }
+  return 0;
+}

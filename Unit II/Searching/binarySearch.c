0e6ea44183a6238c41fb5b746f07584bfc7683c6
AdiChavan08 <130481230+Adityachavan0@users.noreply.github.com>
2026-08-14 13:12:06 +0530

    Refactor binary search into a separate function

--- Unit II/Searching/binarySearch.c
+++ Unit II/Searching/binarySearch.c
@@ -1,11 +1,29 @@
 #include<stdio.h>
 
+int binarySearch(int size,int a[],int target){
+    int first=0,last=size-1,mid;
+    while(first<=last){
+        mid=(first+last)/2;
+        if(a[mid]==target){
+            return mid;
+            break;
+        }
+        if(a[mid]<target){   
+            first =mid+1;
+        }
+        else{
+            last=mid-1;
+        }
+    }
+    return -1;
+}
 int main(){
-    int a[50],size,i,target;
+    int size,i;
     
     printf("Enter size of array:\t");
     scanf("%d",&size);
 
+    int a[size];
     printf("Enter Array Elements in sorting Order:\n");
     for(i=0;i<size;i++){
         printf("%d:",i+1);
@@ -16,29 +34,17 @@ int main(){
     for(i=0;i<size;i++){
         printf("%d\t",a[i]);
     }
-
+    int target;
+    
     printf("\nEnter Value for search:\t");
     scanf("%d",&target);
 
-    int first=0,last=size-1,mid;
-    int flag=0;
-    while(first<=last){
-        mid=(first+last)/2;
-        if(a[mid]==target){
-            printf("Value is present at position %d",mid+1);
-            flag=1;
-            break;
-        }
-        if(a[mid]<target){
-            first =mid+1;
-        }
-        else{
-            last=mid-1;
-        }
-    }
-    if (flag==0)
+    int flag=binarySearch(size,a,target);
+    if (flag>=0)
     {
-        printf("Data is not present");
+        printf("Element presentn at %d index",flag);
     }
-    
+    else{
+        printf("Element not present");
+    }   
 }

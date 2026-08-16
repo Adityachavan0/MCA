4cd40ea34fca1e0b06036d704331a41386d3315f
AdiChavan08 <130481230+Adityachavan0@users.noreply.github.com>
2026-08-16 20:50:33 +0530

    Add bubble sort implementation in bubbleSort.c
    Implement bubble sort algorithm and user input for array.

---
+++ DSA/Sorting/bubbleSort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void bubbleSort(int size,int a[]){
+  for(int i=0;i<size-1;i++){
+    for(int j=0;j<=size-i-1;j++){
+      if(a[j]>a[j+1]){
+        int temp=a[j];
+        a[j]=a[j+1];
+        a[j+1]=temp;
+      }
+    }
+  }
+}
+int main(){
+  int size=0;
+  printf("Enter a size of array: ");
+  scanf("%d",&size);
+  int a[size];
+  printf("Enter value of Array\n");
+  for(int i=0;i<size;i++){
+    printf("%d:",i+1);
+    scanf("%d",&a[i]);
+  }
+  printf("values of array:");
+  for(int i=0;i<size;i++){
+    printf("%d\t",a[i]);
+  }
+  bubbleSort(size,a);
+  printf("\nSorted array:");
+  for(int i=0;i<size;i++){
+    printf("%d\t",a[i]);
+  }
+}
